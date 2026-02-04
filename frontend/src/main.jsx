ef33707b30aa726711bfb4e0f36797d98f3db95b
Aman Soni <amansoni0228@gmail.com>
2026-02-05 02:18:10 +0530

    change backend URL in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "react-toastify/dist/ReactToastify.css";
-export const serverUrl="http://localhost:4000"
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+
+export const serverUrl="https://ai-powered-lms-platform-vfgu.onrender.com"
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
