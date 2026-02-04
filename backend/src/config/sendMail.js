6893e63480baab9a84c8b60052feb49981e09363
Aman Soni <amansoni0228@gmail.com>
2026-02-05 04:33:10 +0530

    update

--- backend/src/config/sendMail.js
+++ backend/src/config/sendMail.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config({ path: './.env' })
+// import dotenv from "dotenv";
+// dotenv.config({ path: './.env' })
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
