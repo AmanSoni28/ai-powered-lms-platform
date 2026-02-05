00416546b35e9b3b731066854f666b359ef3520f
Aman Soni <amansoni0228@gmail.com>
2026-02-05 21:59:26 +0530

    nodemailer fixed

--- backend/src/config/sendMail.js
+++ backend/src/config/sendMail.js
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config({ path: './.env' })
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,     // Use true for port 465, false for port 587
+  service: "gmail",
+  port: 46
+  secure: true, 5,    // Use true for port 465, false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
